73c306f037e54008f2f47857527c333cc9b4b8e5
lahuraj-wadkar <lahuraj.wadkar@gmail.com>
2026-06-11 23:48:52 +0530

    User information code

---
+++ User_info.py
@@ -0,0 +1,7 @@
+name = input("Enter your name : ")
+age = int(input("Enter your age :"))
+
+print("Type of name :", type(name))
+print("Type of age :", type(age))
+
+print(f"Hello {name} , you will be {age+1} next year!")
